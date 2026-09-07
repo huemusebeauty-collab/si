011d5c248c013c4cd7ac44e8e912759f15cb3c76
huemusebeauty-collab <huemusebeauty@gmail.com>
2026-09-07 22:26:24 +0530

    Register product tax service

--- backend/src/modules/products/products.module.ts
+++ backend/src/modules/products/products.module.ts
@@ -3,13 +3,14 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { ProductEntity } from "./entities/product.entity";
 import { ProductVariantEntity } from "./entities/product-variant.entity";
 import { ProductsService } from "./products.service";
+import { ProductTaxService } from "./product-tax.service";
 import { ProductsController } from "./products.controller";
 import { CategoriesModule } from "@/modules/categories/categories.module";
 
 @Module({
   imports: [TypeOrmModule.forFeature([ProductEntity, ProductVariantEntity]), CategoriesModule],
   controllers: [ProductsController],
-  providers: [ProductsService],
-  exports: [ProductsService],
+  providers: [ProductsService, ProductTaxService],
+  exports: [ProductsService, ProductTaxService],
 })
 export class ProductsModule {}
