afef573928a6d0b1463b6c3ea2034f0fb93a6d6a
huemusebeauty-collab <huemusebeauty@gmail.com>
2026-09-07 19:03:01 +0530

    Wire product category service

--- backend/src/modules/products/products.module.ts
+++ backend/src/modules/products/products.module.ts
@@ -4,9 +4,10 @@ import { ProductEntity } from "./entities/product.entity";
 import { ProductVariantEntity } from "./entities/product-variant.entity";
 import { ProductsService } from "./products.service";
 import { ProductsController } from "./products.controller";
+import { CategoriesModule } from "@/modules/categories/categories.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductEntity, ProductVariantEntity])],
+  imports: [TypeOrmModule.forFeature([ProductEntity, ProductVariantEntity]), CategoriesModule],
   controllers: [ProductsController],
   providers: [ProductsService],
   exports: [ProductsService],
