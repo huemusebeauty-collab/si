7851b24ee71cf978cc17f59c061ad78242620151
huemusebeauty-collab <huemusebeauty@gmail.com>
2026-09-09 10:49:56 +0530

    Wire website change intelligence

--- backend/src/modules/website-events/website-events.module.ts
+++ backend/src/modules/website-events/website-events.module.ts
@@ -9,11 +9,13 @@ import { ProductConversionController } from "./product-conversion.controller";
 import { ProductConversionService } from "./product-conversion.service";
 import { CustomerJourneyController } from "./customer-journey.controller";
 import { CustomerJourneyService } from "./customer-journey.service";
+import { WebsiteChangeIntelligenceController } from "./website-change-intelligence.controller";
+import { WebsiteChangeIntelligenceService } from "./website-change-intelligence.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([WebsiteEventEntity])],
-  controllers: [WebsiteEventsController, WebsiteFunnelController, ProductConversionController, CustomerJourneyController],
-  providers: [WebsiteEventsService, WebsiteFunnelService, ProductConversionService, CustomerJourneyService],
-  exports: [WebsiteEventsService, WebsiteFunnelService, ProductConversionService, CustomerJourneyService],
+  controllers: [WebsiteEventsController, WebsiteFunnelController, ProductConversionController, CustomerJourneyController, WebsiteChangeIntelligenceController],
+  providers: [WebsiteEventsService, WebsiteFunnelService, ProductConversionService, CustomerJourneyService, WebsiteChangeIntelligenceService],
+  exports: [WebsiteEventsService, WebsiteFunnelService, ProductConversionService, CustomerJourneyService, WebsiteChangeIntelligenceService],
 })
 export class WebsiteEventsModule {}
