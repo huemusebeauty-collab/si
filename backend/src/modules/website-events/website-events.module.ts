b4736d01985bc55b2b11ae01abeaa9f00db050ba
huemusebeauty-collab <huemusebeauty@gmail.com>
2026-09-09 10:18:59 +0530

    Wire website funnel service

--- backend/src/modules/website-events/website-events.module.ts
+++ backend/src/modules/website-events/website-events.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { WebsiteEventEntity } from "./entities/website-event.entity";
 import { WebsiteEventsController } from "./website-events.controller";
 import { WebsiteEventsService } from "./website-events.service";
+import { WebsiteFunnelController } from "./website-funnel.controller";
+import { WebsiteFunnelService } from "./website-funnel.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([WebsiteEventEntity])],
-  controllers: [WebsiteEventsController],
-  providers: [WebsiteEventsService],
-  exports: [WebsiteEventsService],
+  controllers: [WebsiteEventsController, WebsiteFunnelController],
+  providers: [WebsiteEventsService, WebsiteFunnelService],
+  exports: [WebsiteEventsService, WebsiteFunnelService],
 })
 export class WebsiteEventsModule {}
