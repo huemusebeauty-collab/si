9d82438b6d20822783e9c8e05c756983d58d028a
huemusebeauty-collab <huemusebeauty@gmail.com>
2026-09-19 11:59:09 +0530

    fix(logistics): register order repository

--- backend/src/modules/logistics/logistics.module.ts
+++ backend/src/modules/logistics/logistics.module.ts
@@ -4,11 +4,12 @@ import { LogisticsController } from "./logistics.controller";
 import { LogisticsService } from "./logistics.service";
 import { ShipmentEntity } from "./entities/shipment.entity";
 import { ShipmentEventEntity } from "./entities/shipment-event.entity";
+import { OrderEntity } from "@/modules/orders/entities/order.entity";
 import { OrdersModule } from "@/modules/orders/orders.module";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([ShipmentEntity, ShipmentEventEntity]),
+    TypeOrmModule.forFeature([ShipmentEntity, ShipmentEventEntity, OrderEntity]),
     OrdersModule,
   ],
   controllers: [LogisticsController],
