c803f2278e42703c3075198d100a909b78bf72e9
huemusebeauty-collab <huemusebeauty@gmail.com>
2026-09-19 18:49:30 +0530

    feat(inventory): register movement ledger entity

--- backend/src/modules/products/products.module.ts
+++ backend/src/modules/products/products.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ProductEntity } from "./entities/product.entity";
 import { ProductVariantEntity } from "./entities/product-variant.entity";
+import { InventoryMovementEntity } from "./entities/inventory-movement.entity";
 import { ProductsService } from "./products.service";
 import { ProductTaxService } from "./product-tax.service";
 import { ProductsController } from "./products.controller";
@@ -9,7 +10,7 @@ import { CategoriesModule } from "@/modules/categories/categories.module";
 import { DatabaseModule } from "@/database/database.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductEntity, ProductVariantEntity]), CategoriesModule, DatabaseModule],
+  imports: [TypeOrmModule.forFeature([ProductEntity, ProductVariantEntity, InventoryMovementEntity]), CategoriesModule, DatabaseModule],
   controllers: [ProductsController],
   providers: [ProductsService, ProductTaxService],
   exports: [ProductsService, ProductTaxService],
